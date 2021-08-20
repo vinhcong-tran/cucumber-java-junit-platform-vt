dff98fff37677bcbe54d4b44dfa7de50ff8629b0
Burak Kaygusuz <kaygusuzburak@gmail.com>
2021-08-20 07:53:58 +0300

    Define the suites

--- src/test/java/io/github/burakkaygusuz/CucumberRunner.java
+++ src/test/java/io/github/burakkaygusuz/CucumberRunner.java
@@ -1,7 +1,16 @@
 package io.github.burakkaygusuz;
 
-import io.cucumber.junit.platform.engine.Cucumber;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
 
-@Cucumber
+import static io.cucumber.junit.platform.engine.Constants.*;
+
+@Suite
+@SelectClasspathResource("io/github/burakkaygusuz")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "io.github.burakkaygusuz.step_definitions")
+@ConfigurationParameter(key = PLUGIN_PUBLISH_ENABLED_PROPERTY_NAME, value = "false")
+@ConfigurationParameter(key = PLUGIN_PUBLISH_QUIET_PROPERTY_NAME, value = "true")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
 public class CucumberRunner {
 }
