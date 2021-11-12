6bc5aa5c568ef4661e5fbeb902292357ad5ca4c1
Burak Kaygusuz <kaygusuzburak@gmail.com>
2021-11-12 14:30:09 +0300

    Include the cucumber engine

--- src/test/java/io/github/burakkaygusuz/CucumberRunner.java
+++ src/test/java/io/github/burakkaygusuz/CucumberRunner.java
@@ -1,12 +1,14 @@
 package io.github.burakkaygusuz;
 
 import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
 import static io.cucumber.junit.platform.engine.Constants.*;
 
 @Suite
+@IncludeEngines("cucumber")
 @SelectClasspathResource("io/github/burakkaygusuz")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "io.github.burakkaygusuz.step_definitions")
 @ConfigurationParameter(key = PLUGIN_PUBLISH_ENABLED_PROPERTY_NAME, value = "false")
