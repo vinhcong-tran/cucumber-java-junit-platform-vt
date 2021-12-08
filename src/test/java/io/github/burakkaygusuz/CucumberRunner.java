55d20b8cab1f29587d104dc8d5990166784bf116
Burak Kaygusuz <kaygusuzburak@gmail.com>
2021-12-08 16:50:47 +0300

    Move the config parameters to the properties files

--- src/test/java/io/github/burakkaygusuz/CucumberRunner.java
+++ src/test/java/io/github/burakkaygusuz/CucumberRunner.java
@@ -1,18 +1,11 @@
 package io.github.burakkaygusuz;
 
-import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
-import static io.cucumber.junit.platform.engine.Constants.*;
-
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
-@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "io.github.burakkaygusuz.step_definitions")
-@ConfigurationParameter(key = PLUGIN_PUBLISH_ENABLED_PROPERTY_NAME, value = "false")
-@ConfigurationParameter(key = PLUGIN_PUBLISH_QUIET_PROPERTY_NAME, value = "true")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
 public class CucumberRunner {
 }
