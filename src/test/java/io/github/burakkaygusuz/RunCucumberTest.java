6c589aa1a189a7a8ab32cee31152d41650574343
Burak Kaygusuz <kaygusuzburak@gmail.com>
2021-12-08 16:52:37 +0300

    Rename the runner class

--- src/test/java/io/github/burakkaygusuz/RunCucumberTest.java
+++ src/test/java/io/github/burakkaygusuz/RunCucumberTest.java
@@ -7,5 +7,5 @@ import org.junit.platform.suite.api.Suite;
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
-public class CucumberRunner {
+public class RunCucumberTest {
 }
