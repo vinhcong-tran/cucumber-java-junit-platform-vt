454ee981a7bafb093e715faee738bb6558fa17ce
Burak Kaygusuz <kaygusuzburak@gmail.com>
2021-12-08 16:49:55 +0300

    Fix the classpath resource

--- src/test/java/io/github/burakkaygusuz/CucumberRunner.java
+++ src/test/java/io/github/burakkaygusuz/CucumberRunner.java
@@ -9,7 +9,7 @@ import static io.cucumber.junit.platform.engine.Constants.*;
 
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("io/github/burakkaygusuz")
+@SelectClasspathResource("features")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "io.github.burakkaygusuz.step_definitions")
 @ConfigurationParameter(key = PLUGIN_PUBLISH_ENABLED_PROPERTY_NAME, value = "false")
 @ConfigurationParameter(key = PLUGIN_PUBLISH_QUIET_PROPERTY_NAME, value = "true")
