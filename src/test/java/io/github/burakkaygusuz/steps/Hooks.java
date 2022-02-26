0a28e2e3599752a4e633396d2d910da1f1ed0940
Burak Kaygusuz <kaygusuzburak@gmail.com>
2022-02-26 10:04:00 +0300

    Add the scenario info logs

--- src/test/java/io/github/burakkaygusuz/steps/Hooks.java
+++ src/test/java/io/github/burakkaygusuz/steps/Hooks.java
@@ -2,7 +2,10 @@ package io.github.burakkaygusuz.steps;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
 import io.github.burakkaygusuz.config.BaseDriver;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.Logger;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
@@ -12,6 +15,7 @@ import java.time.Duration;
 
 public class Hooks {
 
+    private static final Logger LOGGER = (Logger) LogManager.getLogger(Hooks.class.getName());
     private final BaseDriver baseDriver;
 
     public Hooks(BaseDriver baseDriver) {
@@ -19,7 +23,8 @@ public class Hooks {
     }
 
     @Before()
-    public void beforeScenario() throws MalformedURLException {
+    public void beforeScenario(Scenario scenario) throws MalformedURLException {
+        LOGGER.info("Scenario " + scenario.getName() + " started");
         baseDriver.setDriver(new RemoteWebDriver(new URL("http://localhost:4444"), new FirefoxOptions()));
 
         baseDriver.getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
@@ -28,8 +33,8 @@ public class Hooks {
     }
 
     @After
-    public void afterScenario() {
+    public void afterScenario(Scenario scenario) {
         baseDriver.getDriver().quit();
+        LOGGER.info("Scenario " + scenario.getName() + " finished.Status: " + scenario.isFailed());
     }
-
 }
