81aa7aa8cf6aa8601ff5034722860be0d0c1daa5
Burak Kaygusuz <kaygusuzburak@gmail.com>
2022-10-05 13:09:08 +0300

    Add some options

--- src/test/java/io/github/burakkaygusuz/steps/Hooks.java
+++ src/test/java/io/github/burakkaygusuz/steps/Hooks.java
@@ -21,7 +21,14 @@ public class Hooks {
     }
 
     @Before
+    public void beforeScenario(Scenario scenario) {
         LOGGER.info("Scenario: " + scenario.getName() + " started");
+        final FirefoxOptions options = new FirefoxOptions();
+        options.addPreference("dom.webnotifications.enabled", false)
+                .addPreference("gfx.direct2d.disabled", true)
+                .addPreference("layers.acceleration.force-enabled", true)
+                .addPreference("javascript.enabled", true);
+        baseDriver.setDriver(new RemoteWebDriver(options, false));
         baseDriver.getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         baseDriver.getDriver().manage().window().maximize();
     }
